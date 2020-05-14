23477221a6a8a34bde50e2ee41508ed2df4dfaa8
Gary Tate <me@garytate.co.uk>
2020-05-14 19:05:20 +0100

    Update sh_metro_dweller.lua

--- ixmetro2033rp/schema/factions/sh_metro_dweller.lua
+++ ixmetro2033rp/schema/factions/sh_metro_dweller.lua
@@ -4,21 +4,27 @@ FACTION.description = "A dweller of the post-apocalyptic Metro tunnels."
 FACTION.color = Color(150, 125, 100, 255)
 FACTION.isDefault = true
 FACTION.models = {
-"models/half-dead/metroll/a1b1.mdl",
-"models/half-dead/metroll/a2b1.mdl",
-"models/half-dead/metroll/a3b1.mdl",
-"models/half-dead/metroll/a4b1.mdl",
-"models/half-dead/metroll/a5b1.mdl",
-"models/half-dead/metroll/f1b1.mdl",
-"models/half-dead/metroll/f3b1.mdl",
-"models/half-dead/metroll/f4b1.mdl",
-"models/half-dead/metroll/f6b1.mdl",
-"models/half-dead/metroll/f7b1.mdl",
-"models/half-dead/metroll/m6b1.mdl",
-"models/half-dead/metroll/m7b1.mdl",
-"models/half-dead/metroll/m8b1.mdl",
-"models/half-dead/metroll/m9b1.mdl",
-"models/half-dead/metroll/m5b1.mdl",
+  "models/half-dead/metrollfix/a1b1.mdl",
+	"models/half-dead/metrollfix/a3b1.mdl",
+	"models/half-dead/metrollfix/a2b1.mdl",
+	"models/half-dead/metrollfix/a4b1.mdl",
+	"models/half-dead/metrollfix/a5b1.mdl",
+	"models/half-dead/metrollfix/m2b1.mdl",
+	"models/half-dead/metrollfix/m4b1.mdl",
+	"models/half-dead/metrollfix/m5b1.mdl",
+	"models/half-dead/metrollfix/m6b1.mdl",
+	"models/half-dead/metrollfix/m7b1.mdl",
+	"models/half-dead/metrollfix/m8b1.mdl",
+	"models/half-dead/metrollfix/m9b1.mdl",
+	"models/half-dead/metrollfix/a6b1.mdl",
+	"models/half-dead/metrollfix/m3b1.mdl",
+	"models/half-dead/metrollfix/m1b1.mdl",
+	"models/half-dead/metrollfix/f7b1.mdl",
+	"models/half-dead/metrollfix/f6b1.mdl",
+	"models/half-dead/metrollfix/f4b1.mdl",
+	"models/half-dead/metrollfix/f3b1.mdl",
+	"models/half-dead/metrollfix/f2b1.mdl",
+	"models/half-dead/metrollfix/f1b1.mdl"
 }
 
 FACTION_METRO = FACTION.index
